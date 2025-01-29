35790e7beb480e3108c5404f45131de529921e48
PauloJacko <paulo.millanh@gmail.com>
2025-01-29 16:02:59 -0300

    Todas las config listas para empezar pag web

--- opticalcm/opticalcm/settings.py
+++ opticalcm/opticalcm/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'opticalcm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+         'DIRS': [
+            BASE_DIR / 'opticalcm' / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
