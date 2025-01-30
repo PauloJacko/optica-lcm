ab59f208e5eae703dc5942aa40e8d6cf3ff9d663
PauloJacko <paulo.millanh@gmail.com>
2025-01-30 16:18:30 -0300

    Primeros cambios

--- opticalcm/opticalcm/urls.py
+++ opticalcm/opticalcm/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.inicio, name='inicio'),  
+    path('servicios/', views.servicios, name='servicios'),
+    path('quienes_somos/', views.quienes_somos, name='quienes_somos'),
+    path('convenios/', views.convenios, name='convenios'),
 ]
 
--- opticalcm/opticalcm/views.py
+++ opticalcm/opticalcm/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'inicio.html')
+
+def servicios(request):
+    return render(request, 'servicios.html')
+
+def quienes_somos(request):
+    return render(request, 'quienes_somos.html')
+
+def convenios(request):
+    return render(request, 'convenios.html')
